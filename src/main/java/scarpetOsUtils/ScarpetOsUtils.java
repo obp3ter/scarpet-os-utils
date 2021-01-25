346f4337e66c82cd84352fbc10202a408f4fa388
obp3ter <15703843+obp3ter@users.noreply.github.com>
2021-01-25 15:15:53 +0200

    removed unnecessary dependencies and added ci

--- src/main/java/scarpetOsUtils/ScarpetOsUtils.java
+++ src/main/java/scarpetOsUtils/ScarpetOsUtils.java
@@ -3,18 +3,16 @@ package scarpetOsUtils;
 import carpet.CarpetExtension;
 import carpet.CarpetServer;
 import carpet.script.CarpetExpression;
-import lombok.extern.slf4j.Slf4j;
 import net.fabricmc.api.ModInitializer;
 import scarpetOsUtils.script.GetOs;
 import scarpetOsUtils.script.OsExecutor;
 
-@Slf4j
 public class ScarpetOsUtils implements CarpetExtension, ModInitializer {
 
     @Override
     public void onInitialize() {
         CarpetServer.manageExtension(this);
-        log.info("Scarpet Os Utils Loaded");
+        System.out.println("Scarpet Os Utils loaded");
     }
     @Override
     public void scarpetApi(CarpetExpression expression){
